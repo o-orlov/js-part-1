98a096169ee566575be368b43e111bd5f27f6716
Oleg Orlov <o.orlov@outlook.com>
2022-12-25 00:24:33 +0300

    Implement client for REST Countries service

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-async function getData(url) {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        redirect: 'follow',
-    });
-    return response.json();
-}
+class CountriesClient {
+    constructor() {
+        this.baseUrl = 'https://restcountries.com/v3.1';
+        this.urls = {
+            all: '/all?fields=name,cca3,area',
+            code: '/alpha/{code}?fields=borders',
+        };
+    }
 
-async function loadCountriesData() {
-    const countries = await getData('https://restcountries.com/v3.1/all?fields=name,cca3,area');
-    return countries.reduce((result, country) => {
-        result[country.cca3] = country;
-        return result;
-    }, {});
+    getUrl(urlName, urlParams) {
+        let url = this.urls[urlName];
+        if (urlParams) {
+            Object.keys(urlParams).forEach((paramName) => {
+                url = url.replace(`{${paramName}}`, urlParams[paramName]);
+            });
+        }
+        return this.baseUrl + url;
+    }
+
+    async get(urlName, urlParams) {
+        const url = this.getUrl(urlName, urlParams);
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+        });
+        return response.json();
+    }
+
+    async getCountriesData() {
+        const data = await this.get('all');
+        return data.reduce((result, country) => {
+            result[country.cca3] = country;
+            return result;
+        }, {});
+    }
+
+    async getNeighbours(countryCode) {
+        const data = await this.get('code', { code: countryCode });
+        return data.borders;
+    }
 }
 
 const form = document.getElementById('form');
@@ -24,6 +49,7 @@ const toCountry = document.getElementById('toCountry');
 const countriesList = document.getElementById('countriesList');
 const submit = document.getElementById('submit');
 const output = document.getElementById('output');
+const countriesClient = new CountriesClient();
 
 (async () => {
     fromCountry.disabled = true;
@@ -31,8 +57,11 @@ const output = document.getElementById('output');
     submit.disabled = true;
 
     output.textContent = 'Loading…';
-    const countriesData = await loadCountriesData();
+    const countriesData = await countriesClient.getCountriesData();
     output.textContent = '';
+
+    // TODO: Удалить тестовый вывод запроса соседних с РФ стран
+    console.log(await countriesClient.getNeighbours('RUS'));
 
     // Заполняем список стран для подсказки в инпутах
     Object.keys(countriesData)
