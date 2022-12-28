879344c10d8447b7f3c46ca2808ce8561d12bb66
Oleg Orlov <o.orlov@outlook.com>
2022-12-28 21:46:05 +0300

    Find route

--- index.js
+++ index.js
@@ -48,14 +48,15 @@ class CountriesClient {
 
 class CountriesService {
     constructor() {
-        this.countriesClient = new CountriesClient();
+        this.client = new CountriesClient();
         this.countriesData = null;
         this.countryNameToCodeMapping = null;
+        this.countryCodeToNeighboursMapping = {};
     }
 
     async getCountriesData() {
         if (this.countriesData === null) {
-            const data = await this.countriesClient.getAll(['name', 'cca3', 'area']);
+            const data = await this.client.getAll(['name', 'cca3', 'area']);
             this.countriesData = data.reduce((result, country) => {
                 result[country.cca3] = country;
                 return result;
@@ -80,20 +81,94 @@ class CountriesService {
     }
 
     async getNeighboursByCountryCode(countryCode) {
-        const data = await this.countriesClient.searchByCountryCode(countryCode, ['borders']);
-        return data.borders;
+        if (this.countryCodeToNeighboursMapping[countryCode] === undefined) {
+            const data = await this.client.searchByCountryCode(countryCode, ['borders']);
+            this.countryCodeToNeighboursMapping[countryCode] = data.borders;
+        }
+        return this.countryCodeToNeighboursMapping[countryCode];
     }
 
-    async getNeighboursByCountryName(countryName) {
-        const data = await this.countriesClient.searchByCountryName(countryName, ['borders']);
-        return data.borders;
+    async getNeighboursByCountryCodes(countryCodes) {
+        const promises = countryCodes.map(this.getNeighboursByCountryCode.bind(this));
+        return Promise.allSettled(promises);
+    }
+}
+
+class RouteVariantTreeItem {
+    constructor(countryCode, parent) {
+        this.countryCode = countryCode;
+        this.parent = parent;
+        this.children = [];
     }
 
-    async calculateRoute(fromCountryName, toCountryName) {
-        const fromCountryCode = await this.getCountryCodeByName(fromCountryName);
-        const toCountryCode = await this.getCountryCodeByName(toCountryName);
-        // TODO: Вернуть найденный маршрут или выдать ошибку.
-        return [fromCountryName, toCountryName];
+    appendChild(child) {
+        this.children.push(child);
+    }
+}
+
+class RouteVariantTree {
+    constructor(countryCode) {
+        this.root = new RouteVariantTreeItem(countryCode, null);
+    }
+}
+
+class RouteFinder {
+    constructor(countriesService) {
+        this.countriesService = countriesService;
+        this.maxIterations = 10;
+    }
+
+    async findNeighbour(parents, countryCode, iteration = 1) {
+        let found = false;
+
+        const countryCodes = parents.map((item) => item.countryCode);
+        const results = await this.countriesService.getNeighboursByCountryCodes(countryCodes);
+        results.forEach((result, index) => {
+            if (result.status === 'fulfilled') {
+                const parent = parents[index];
+                const neighbours = result.value;
+                neighbours.forEach((neighbour) => {
+                    if (neighbour === countryCode) {
+                        found = true;
+                    }
+                    const child = new RouteVariantTreeItem(neighbour, parent);
+                    parent.appendChild(child);
+                });
+            } else {
+                throw new Error(result.reason);
+            }
+        });
+
+        if (!found && iteration < this.maxIterations) {
+            const children = [];
+            parents.forEach((parent) => children.concat(parent.children));
+            return this.findNeighbour(children, countryCode, iteration + 1);
+        }
+
+        return found;
+    }
+
+    async findRoute(fromCountryName, toCountryName) {
+        if (!fromCountryName) {
+            throw new Error('Departure country cannot be empty.');
+        }
+        if (!toCountryName) {
+            throw new Error('Destination country cannot be empty.');
+        }
+        if (fromCountryName === toCountryName) {
+            throw new Error('Departure and destination countries are the same.');
+        }
+
+        const fromCountryCode = await this.countriesService.getCountryCodeByName(fromCountryName);
+        const toCountryCode = await this.countriesService.getCountryCodeByName(toCountryName);
+        const tree = new RouteVariantTree(fromCountryCode);
+
+        if (await this.findNeighbour([tree.root], toCountryCode)) {
+            // TODO: Найти все валидные варианты маршрутов и вернуть их в строковом или JSON представлении.
+            return `Found route from ${fromCountryName} to ${toCountryName}.`;
+        }
+
+        return null;
     }
 }
 
@@ -105,6 +180,7 @@ const submit = document.getElementById('submit');
 const output = document.getElementById('output');
 
 const countriesService = new CountriesService();
+const routeFinder = new RouteFinder(countriesService);
 
 function setInteractionDisabled(disabled) {
     fromCountry.disabled = disabled;
@@ -117,7 +193,7 @@ function showMessage(message) {
 }
 
 function showError(error) {
-    output.textContent = `Error! ${error.message}`;
+    output.textContent = `Error: ${error.message}`;
 }
 
 function clearMessage() {
@@ -146,19 +222,17 @@ function clearMessage() {
         event.preventDefault();
         clearMessage();
         setInteractionDisabled(true);
-        showMessage(`Calculating route from ${fromCountry.value} to ${toCountry.value}…`);
-        countriesService
-            .calculateRoute(fromCountry.value, toCountry.value)
-            .then((route) => {
-                showMessage(route.join(' → '));
-            })
-            .catch((reason) => {
-                if (reason instanceof Error) {
-                    showError(reason);
+        showMessage(`Finding route from ${fromCountry.value} to ${toCountry.value}…`);
+        routeFinder
+            .findRoute(fromCountry.value, toCountry.value)
+            .then((result) => {
+                if (result !== null) {
+                    showMessage(result);
                 } else {
-                    console.log(reason);
+                    showMessage(`Route from ${fromCountry.value} to ${toCountry.value} not found.`);
                 }
             })
+            .catch(showError)
             .finally(() => {
                 setInteractionDisabled(false);
             });
