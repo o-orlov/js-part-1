a0acb77a61d6cc07696aa01f53795a9dc254a871
Oleg Orlov <o.orlov@outlook.com>
2023-01-08 14:48:38 +0300

    Rework URL constructing

--- CountriesClient.js
+++ CountriesClient.js
@@ -1,6 +1,7 @@
 class CountriesClient {
-    static #BASE_URL = 'https://restcountries.com/v3.1';
-    static #URLS = {
+    static #BASE_URL = 'https://restcountries.com';
+    static #API_VERSION = 'v3.1';
+    static #PATHNAMES = {
         all: '/all',
         code: '/alpha/{code}',
     };
@@ -18,21 +19,24 @@ class CountriesClient {
     }
 
     static #getUrl(urlName, urlParams, fields) {
-        let url = CountriesClient.#URLS[urlName];
+        let pathname = CountriesClient.#PATHNAMES[urlName];
         if (urlParams) {
             Object.keys(urlParams).forEach((paramName) => {
-                url = url.replace(`{${paramName}}`, urlParams[paramName]);
+                pathname = pathname.replace(`{${paramName}}`, urlParams[paramName]);
             });
         }
+        const url = new URL(`${CountriesClient.#API_VERSION}${pathname}`, CountriesClient.#BASE_URL);
         if (fields) {
-            url += `?fields=${fields.toString()}`;
+            for (const field of fields) {
+                url.searchParams.append('fields', field);
+            }
         }
-        return CountriesClient.#BASE_URL + url;
+        return url;
     }
 
     async #get(urlName, urlParams, fields) {
         const url = CountriesClient.#getUrl(urlName, urlParams, fields);
-        let data = this.#cache[url];
+        let data = this.#cache[url.href];
         if (data !== undefined) {
             return data;
         }
@@ -48,7 +52,7 @@ class CountriesClient {
             throw new Error(response.statusText);
         }
         data = response.json();
-        this.#cache[url] = data;
+        this.#cache[url.href] = data;
         return data;
     }
 
